f1432fa361a0da10a7ee763b55c87a44b79687b7
fabianzielinski <fabian.zielinski@o2.pl>
2021-04-08 22:25:39 +0200

    Add components Review and Navigation to section Hero

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navigation from "./components/Navigation";
+// import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Navigation />
         <Hero />
       </div>
     );
